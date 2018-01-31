515b4eb4250a076a2896773112413a4ca0be134f
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-01-31 22:47:32 +0100

    opening window

--- src/main.cpp
+++ src/main.cpp
@@ -4,57 +4,101 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
+#include <string>
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>  // EXIT_FAILURE
 
-const int ARGUMENT_LEVEL_INDEX = 2;
-const int LEVEL_COUNT = 1;
+namespace ost {
+const int LEVEL_COUNT  = 1;
+const int OPENGL_MAJOR = 4;
+const int OPENGL_MINOR = 1;
 
-#define PANIC(msg) { printf("----- PANIC -----| file: %s | line: %d | msg: %s",  __FILE__, __LINE__, msg); exit(EXIT_FAILURE); }
+const char* WIN_NAME = "Overkill Studio - Assignment1";
+const int WIN_WIDTH = 1024;
+const int WIN_HEIGHT = 768;
+}
+
+#define PANIC(msg) {\
+            fprintf(stderr,\
+                "----- PANIC -----| file: %s | line: %d | msg: %s\n", __FILE__, __LINE__,  msg);\
+            getchar();\
+            exit(EXIT_FAILURE);}\
 
 int main(int argc, char* argv[]) {
 
-    int selectedLevel = 0;
+    int _selectedLevel = 0;
 
     // PROCESS ARGUMENTS
-    { 
-
-    if (argc > ARGUMENT_LEVEL_INDEX) {
+    if (argc > 2) {
         PANIC("Too many arguments from command-line");
     }
-    if (argc == ARGUMENT_LEVEL_INDEX) {
-        selectedLevel = atoi(argv[1]);
+    if (argc == 2) {
+        _selectedLevel = atoi(argv[1]);
     }
-    if (selectedLevel < 0 || selectedLevel >= LEVEL_COUNT) {
+    if (_selectedLevel < 0 || _selectedLevel >= ost::LEVEL_COUNT) {
         PANIC("Level not available");
     }
+    const int selectedLevel = _selectedLevel;
     printf("Starting level %d\n", selectedLevel);
 
+    // INITIALIZE G L F W
+    if (!glfwInit()){
+        glfwTerminate();
+        PANIC("Failed to initialize GLFW");
     }
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, ost::OPENGL_MAJOR);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, ost::OPENGL_MINOR);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // INIT OPENGL 
-
-    // INIT WINDOW1
-
+    // OPEN G L F W WINDOW AND HOOK UP THE OPEN GL CONTEXT
+    GLFWwindow* window = glfwCreateWindow(ost::WIN_WIDTH, ost::WIN_HEIGHT, ost::WIN_NAME, NULL, NULL);
+    glfwMakeContextCurrent(window);
+    if (window == NULL) {
+        glfwTerminate();
+        PANIC("Failed to open GLFW window");
+    }
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    glfwSwapInterval(1);
+    
+    // INITIALIZE G L E W
+    glewExperimental = GL_TRUE;  // MACOS/intel cpu support
+    if (glewInit() != GLEW_OK) {
+        glfwTerminate();
+        PANIC("Failed to initialize GLEW");
+    }
+    
     // LOAD ASSETS (map, spritesheet)
 
-    // BUILD ASSETS STRUCTS
+    // BUILD ASSET-STRUCTS
 
     // GENERATE GPU BUFFERS
+    //GLuint vao[2];
 
     // GENERATE MATRICES
 
+    bool running = true;
+    glClearColor(1,1,1,0);
+
     // GAMELOOP
+    while (running) {
 
         // INPUT
-
+        glfwPollEvents();
+        running = (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+                   glfwWindowShouldClose(window) == 0);    
+        
         // UPDATE 
-
         // RENDER
-
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
 
     // CLEANUP
-
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
