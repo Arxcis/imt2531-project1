c5a8224151e6d881ec98a4d1c7649ad9529eed22
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-01-31 17:53:20 +0100

    psudocode for project

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,34 @@
 
+int main(int argc, char** argv) {
 
-int main() {
+    // How many arguments do we support?
+    const int SUPPORTED_NUMBER_OF_ARGUMENTS = 2;
+    if (argc > SUPPORTED_NUMBER_OF_ARGUMENTS) 
+        return 0;
+
+
+    // INIT OPENGL 
+
+    // INIT WINDOW
+
+    // LOAD ASSETS (map, spritesheet)
+
+    // BUILD ASSETS STRUCTS
+
+    // GENERATE GPU BUFFERS
+
+    // GENERATE MATRICES
+
+    // GAMELOOP
+
+        // INPUT
+
+        // UPDATE 
+
+        // RENDER
+
+
+    // CLEANUP
+    
     return 0;
 }
