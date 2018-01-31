bdd19ec18ac1572b6413e298b77fcfd3a49c2f65
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-01-31 21:08:00 +0100

    Added PANIC macro

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,37 @@
 #include <stdio.h>
 #include <errno.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>  // EXIT_FAILURE
+
+const int ARGUMENT_LEVEL_INDEX = 2;
+const int LEVEL_COUNT = 1;
+
+#define PANIC(msg) { printf("----- PANIC -----| file: %s | line: %d | msg: %s",  __FILE__, __LINE__, msg); exit(EXIT_FAILURE); }
 
 int main(int argc, char* argv[]) {
 
+    int selectedLevel = 0;
+
     // PROCESS ARGUMENTS
-    const int SUPPORTED_NUMBER_OF_ARGUMENTS = 2;
-    assert(argc <= SUPPORTED_NUMBER_OF_ARGUMENTS);
-    printf("Argv 1: %s\n", argv[1]);
+    { 
+
+    if (argc > ARGUMENT_LEVEL_INDEX) {
+        PANIC("Too many arguments from command-line");
+    }
+    if (argc == ARGUMENT_LEVEL_INDEX) {
+        selectedLevel = atoi(argv[1]);
+    }
+    if (selectedLevel < 0 || selectedLevel >= LEVEL_COUNT) {
+        PANIC("Level not available");
+    }
+    printf("Starting level %d\n", selectedLevel);
+
+    }
 
     // INIT OPENGL 
 
