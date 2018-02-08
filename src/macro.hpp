9569cf874085532619ad1bd99d2e77e58c40878a
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-02-08 13:18:33 +0100

    new macro.hpp

---
+++ src/macro.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+
+#define PANIC(msg) {\
+            fprintf(stderr,\
+                "----- PANIC -----| file: %s | line: %d | msg: %s\n", __FILE__, __LINE__,  msg);\
+            getchar();\
+            exit(EXIT_FAILURE);}\
