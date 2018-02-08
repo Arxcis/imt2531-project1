d8eb563c6b7683ebb4fe871bf681361161a20970
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-02-08 13:19:40 +0100

    started merging main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,12 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
 
+#include "./macro.hpp"
 #include "./loadShader.hpp"
+#include "./loadLevel.hpp"
 #include "./loadTexture.hpp"
+#include "./Pacman.hpp"
+#include "./spritesheet.hpp"
 
 
 //DISCUSSION: using Color = float[4]; instead?
@@ -40,14 +44,10 @@ const int WIN_WIDTH   = 1024;
 const int WIN_HEIGHT  = 1024;
 }
 
-#define PANIC(msg) {\
-            fprintf(stderr,\
-                "----- PANIC -----| file: %s | line: %d | msg: %s\n", __FILE__, __LINE__,  msg);\
-            getchar();\
-            exit(EXIT_FAILURE);}\
-
-
 void update(const GLFWwindow*);
+inline void update(GLFWwindow* window, bool& running, ost::Pacman& pacman, double& keySpaceDeadtime);
+inline void render(GLFWwindow* window, ost::Pacman& pacman, GLuint vbo); 
+
 
 int main(int argc, char* argv[]) {
 
@@ -94,45 +94,10 @@ int main(int argc, char* argv[]) {
         glfwTerminate();
         PANIC("Failed to initialize GLEW");
     }
-
-
-        // LOAD ASSETS (map, spritesheet)
-
-        //READ MAP
-        std::vector<glm::vec2> buffer;
-        {
-            const char* fname = "./levels/level0";
-            std::ifstream in;
-            in.open(fname, std::ios::binary);
-
-            if (in.fail()) {
-                std::cerr << "Unable to open " << fname << " I'm out!" << std::endl;
-                exit(-1);
-            }
-
-            // READ LEVEL SIZES
-            std::string sizeDefs;
-            std::getline(in, sizeDefs);
-            std::size_t delimIndex =  sizeDefs.find("x");
-            std::size_t levelWidth = std::stoi(sizeDefs.substr(0, delimIndex));
-            std::size_t levelHeight = std::stoi(sizeDefs.substr(delimIndex+1, sizeDefs.size()-delimIndex+1));
-
-
-            std::string line;
-            for(float y=0; y < levelHeight ;y++) {
-                if (in.eof() || in.bad()) {
-                    PANIC("Y out of range in level read");
-                }
-                std::getline(in, line);
-                std::stringstream ss(line);
-
-                for(float x=0;x < levelWidth;x++) {
-                    int n;
-                    if(!(ss >> n)) {PANIC("X out of range in level read");} //failed to read, must be end of line
-                    if(n % 2 == 0) { buffer.push_back(glm::vec2((x/levelWidth)-.5f, ((levelHeight-y)/levelHeight)-.5f) * 2.0f); } //FILL THE BUFFER WITH Vectors - vertex candidate
-                }
-            }
-        }
+    
+    // LOAD ASSETS (map, spritesheet)
+    //READ MAP
+    std::vector<glm::vec2> buffer = ost::loadLevel("./levels/level0");
 
     // BUILD ASSET-STRUCTS
 
