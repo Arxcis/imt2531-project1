edbefcc8d2c2c0b612e0cb47ad3b43c0499ba878
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-02-13 05:04:22 +0100

    Shader.hpp - big refactor of render code - bonus: now rendering cheese (red cheese) - bug: Rendering up to 4 ghosts + player. , but animation only works, when there is only Pacman in the buffer.

--- shaders/general.vert
+++ shaders/general.vert
@@ -1,16 +1,26 @@
 #version 410
+
 in vec2 position;
+in vec4 color;
 in vec2 texcoord;
+
+out vec4 Color; 
 out vec2 Texcoord;
 
-uniform mat4 view = mat4(
+uniform mat4 scale = mat4(
 1.0/14.0,   0,          0,  0,
 0,          1.0/14.0,   0,  0,
 0,          0,          1,  0,
 0,          0,          0,  1);
 
+
+uniform float pointSize = 1.0;
+
 void main()
 {
+    gl_Position = scale * vec4(position, 0.0, 1.0);
+    gl_PointSize = pointSize;
+
+    Color = color;
     Texcoord = texcoord;
-    gl_Position = view * vec4(position, 0.0, 1.0);
 }
--- src/Level.hpp
+++ src/Level.hpp
@@ -2,21 +2,34 @@
 #include <vector>
 #include <algorithm>
 #include "glm/glm.hpp"
+#include "./Shader.hpp"
 
 namespace ost
 {
-    using namespace glm;
+    using namespace glm;    
     struct Level {
+        
         Level(const std::vector<vec2> _vertices, const ivec2 _size)
         :vertices(_vertices)
         {
             size = _size;
             biggestSize = (_size.x > _size.y) ? _size.x : _size.y;
+
+          //  for (auto v : vertices)
+            //    std::cout << "v.x :  " << v.x  << "  v.y" << v.y << '\n';
         }
         //
         // BUFFER COMPONENT
         //
         const std::vector<vec2> vertices;
+
+        void bindBufferVertices(const std::vector<Vertex>::iterator vertexBufferIterator) const {
+            auto it = vertexBufferIterator;
+            for(auto v : vertices) {
+                (*it).position = v;
+                it++;
+            }
+        }
 
         //TODO consider changing vertices to ivec2 as they should be in world space units
         int biggestSize;
@@ -27,12 +40,12 @@ namespace ost
         // }
 
         // check if the coordinate is walkable floor
-        bool isWalkable(const vec2 coordinate) {
+        bool isWalkable(const vec2 coordinate) const {
             return std::find_if(
                 vertices.begin(),
                 vertices.end(),
                 [coordinate](vec2 _lvlCoord){
-                    return distance(_lvlCoord, coordinate) < 0.5f;
+                    return distance(_lvlCoord, coordinate) < 0.8f;
                 }
             ) != vertices.end();
         }
